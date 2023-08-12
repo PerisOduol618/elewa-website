0ce3b4cea31afd9f65bc3971b64fdd664789dd08
Oduolperis <oduolpepe618@gmail.com>
2023-08-12 23:32:33 +0300

    Created simple button component

---
+++ libs/elements/layout/buttons/src/lib/components/ButtonsModule/buttons-module.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-buttons-module',
+  templateUrl: './buttons-module.component.html',
+  styleUrls: ['./buttons-module.component.scss'],
+})
+export class ButtonsModuleComponent {}
--- libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
+++ libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonsModuleComponent } from './components/buttons-module/buttons-module.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ButtonsModuleComponent],
 })
 export class ElementsLayoutButtonsModule {}
