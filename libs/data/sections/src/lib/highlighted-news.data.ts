27925adcc59c1bcabb057ff04a5d2d43acad4720
Oduol Peris <oduolpepe618@gmail.com>
2023-08-25 12:38:07 +0300

    ✨feat:mock news items are defined for the component and placed in highlighted-news.data.ts

---
+++ libs/data/sections/src/lib/highlighted-news.data.ts
@@ -0,0 +1,9 @@
+import { NewsItem } from '@elewa-website/models/schema/ui/cards';
+
+export const __highlightedNews: NewsItem[] = [
+  {
+    title: 'Conversational lerning ipsum dolar',
+    description:'Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam dolorem ipsum quia.',
+    buttonText: 'Read More',
+  },
+];
