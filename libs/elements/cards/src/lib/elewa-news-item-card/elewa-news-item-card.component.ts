2ee2277cd835781c5c234bb6dd71486a170e7ac8
Oduol Peris <oduolpepe618@gmail.com>
2023-08-25 12:14:24 +0300

    🎨feat:Generate a  elewa-news-item-card component

---
+++ libs/elements/cards/src/lib/elewa-news-item-card/elewa-news-item-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { NewsItem } from '@elewa-website/models/schema/ui/cards'
+
+@Component({
+  selector: 'elewa-website-elewa-news-item-card',
+  templateUrl: './elewa-news-item-card.component.html',
+  styleUrls: ['./elewa-news-item-card.component.scss'],
+})
+export class ElewaNewsItemCardComponent {}
